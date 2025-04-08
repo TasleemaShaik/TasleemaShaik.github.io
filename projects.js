56cacc02faf3bcf793c674d2a6f4dd44241b2bb3
Tasleema Shaik Mohammed <tshaikmohamm@binghamton.edu>
2025-04-07 23:54:44 -0400

    Add new styles for the door and update door image, also update new project

--- projects.js
+++ projects.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   const projects = [
     {
-      name: "LaunchPadClan4",
-      url: "https://github.com/TasleemaShaik/LaunchPadClan4/blob/main/Chapter6/Assignment-1.mp4",
-      description: `A mini 3D game I did as a part of Outscal's Launchpad program. Launchpad is a 15-day long scholarship program where you learn to build a game from scratch. Best fit for those who want to explore careers in game development. This scholarship is 100% free on selection, where you learn and build a game from scratch in live interactive sessions with Outscal mentors. Launchpad is the only way to get admission in our 7 months job guarantee program : <a href="https://bit.ly/3uKDY8KA" target="_blank">Link</a>`,
-      image: "assets/images/LaunchPadClan4.png"
+      name: "Code AVP",
+      url: "https://github.com/prembhajaj/HackBU",
+      description: `<ul>
+        <li>Developed a full-stack application during the HackBU hackathon using React.js for the frontend and Java Spring Boot for the backend.</li>
+        <li>Created a code editor interface where users can write code, which is then processed to generate UML diagrams for visual representation.</li>
+        <li>Integrated OpenAI's ChatGPT to analyze the code and provide specific improvement suggestions, enhancing code quality.</li>
+        <li>Combined modern web development technologies and AI to create an intuitive and efficient platform.</li>
+        <li>Successfully collaborated in a team environment to deliver an innovative project, receiving positive feedback for its practical application.</li>
+      </ul>`,
+      image: "assets/images/HackBU.png"
     },
     {
-      name: "Tic-Tac-Toe",
-      url: "https://github.com/TasleemaShaik/Projects/tree/master/Tic-Tac-Toe",
-      description: `Tic-Tac-Toe is one of the simple game, which is also called as noughts and crosses but in our local terms we called it as cross-zero game. Other may say differently this game but things are same. It is a pencil-and-paper game for two players, where player select X and O, as their playing item. It is played on the grid of 3×3. The player who succeeds in placing three respective marks in a horizontal, vertical, or diagonal row wins the game.`,
-      image: "assets/images/Tic-Tac-Toe.jpg"
+      "name": "OEVthDistraction",
+      "url": "https://github.com/josharsh/100LinesOfCode/tree/master/OnlineExamWithADistraction",
+      "description": "A concise web-based quiz application developed in under 100 lines of code as part of the 100LinesOfCode challenge. This single-page app features a full-screen YouTube video background to create a distracting environment, testing the user's focus. Questions are dynamically loaded from a JSON file, and users receive immediate feedback with their final score and a review of any incorrect answers.",
+      "image": "assets/images/OnlineExamWithADistraction.png"
+    },    
+    {
+      name: "LaunchPadClan4",
+      url: "https://github.com/TasleemaShaik/LaunchPadClan4/blob/main/Chapter6/Assignment-1.mp4",
+      "description": "A mini 3D game I did as a part of Outscal's Launchpad program. Launchpad is a 15-day long scholarship program where you learn to build a game from scratch. This game was built in Unity and explores mechanics like player movement, collectibles, physics interactions, and scene transitions. Each chapter incrementally added new features such as animations, UI design, and collision-based win logic—culminating in a polished, interactive mini-game experience.",
+      image: "assets/images/LaunchPadClan4.png"
     },
     {
       name: "InstaDiff",
@@ -25,22 +37,16 @@ document.addEventListener("DOMContentLoaded", function () {
       image: "assets/images/InstaDiff.png"
     },
     {
-      name: "Code Analysis & Visualization Platform",
-      url: "https://github.com/prembhajaj/HackBU",
-      description: `<ul>
-        <li>Developed a full-stack application during the HackBU hackathon using React.js for the frontend and Java Spring Boot for the backend.</li>
-        <li>Created a code editor interface where users can write code, which is then processed to generate UML diagrams for visual representation.</li>
-        <li>Integrated OpenAI's ChatGPT to analyze the code and provide specific improvement suggestions, enhancing code quality.</li>
-        <li>Combined modern web development technologies and AI to create an intuitive and efficient platform.</li>
-        <li>Successfully collaborated in a team environment to deliver an innovative project, receiving positive feedback for its practical application.</li>
-      </ul>`,
-      image: "assets/images/HackBU.png"
-    },
-    {
-      name: "News-Articles-Text-Summarizer",
+      name: "News-ATS",
       url: "https://github.com/TasleemaShaik/Projects/tree/master/News-Articles-Text-Summarizer",
       description: "A machine learning tool that summarizes lengthy news articles into concise summaries using Python summarization tools.",
       image: "assets/images/NewsArticleTextSummarizer.jpg"
+    },
+    {
+      name: "Tic-Tac-Toe",
+      url: "https://github.com/TasleemaShaik/Projects/tree/master/Tic-Tac-Toe",
+      description: `Tic-Tac-Toe is one of the simple game, which is also called as noughts and crosses but in our local terms we called it as cross-zero game. Other may say differently this game but things are same. It is a pencil-and-paper game for two players, where player select X and O, as their playing item. It is played on the grid of 3×3. The player who succeeds in placing three respective marks in a horizontal, vertical, or diagonal row wins the game.`,
+      image: "assets/images/Tic-Tac-Toe.jpg"
     }
   ];
 
